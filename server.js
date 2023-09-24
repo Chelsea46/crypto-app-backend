492c49e0a492f313113df780a7e1ba425ee88d5a
Chelsea46 <101319968+Chelsea46@users.noreply.github.com>
2023-09-24 15:17:19 +0100

    attempt 2

--- server.js
+++ server.js
@@ -12,47 +12,44 @@ const corsOptions = {
     origin: 'https://chelsea-cryptoapp.netlify.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-    
 };
-  
+
 app.use(cors(corsOptions));
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
-
 
 db.start.connect((error) => {
     if (error) {
         console.log(error);
     } else {
         console.log('MYSQL CONNECTED')
+
+        const createTableQuery = `
+            CREATE TABLE IF NOT EXISTS users (
+                id int NOT NULL AUTO_INCREMENT,
+                name varchar(255) NOT NULL,
+                email varchar(255) NOT NULL,
+                password varchar(255) NOT NULL,
+                PRIMARY KEY (id)
+            ) 
+        `;
+
+        db.start.query(createTableQuery, (err, results) => {
+            if (err) {
+                console.error('Error creating table:', err);
+            } else {
+                console.log('Table created successfully');
+            }
+
+            db.start.end(); // Close the connection 
+        });
     }
-})
-
-const createTableQuery = `
-CREATE TABLE users (
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	email varchar(255) NOT NULL,
-	password varchar(255) NOT NULL,
-	PRIMARY KEY (id)
-) 
-`;
-
-connection.query(createTableQuery, (err, results) => {
-    if (err) {
-      console.error('Error creating table:', err);
-    } else {
-      console.log('Table created successfully');
-    }
-
-    connection.end(); 
-  });
-
+});
 
 // Define routes
 app.use('/auth', require('./routes/authRoutes'));
 
 app.listen(5000, () => {
-  console.log(`Server is running on port 5000.`);
+    console.log(`Server is running on port 5000.`);
 });
